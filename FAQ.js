a69c423dc41af365ec165e2c978038a8afcd6c47
elinhanstorp <elin.hanstorp@live.com>
2018-12-06 19:37:59 +0100

    Nice design on questions in FAQ

--- FAQ.js
+++ FAQ.js
@@ -1,23 +1,28 @@
 window.onload = function () {
 
+
   const BASE_DIV = document.getElementById('faq_info_coming_from_FAQ.js')
 
   fetch('https://www.teknikfokus.se/FAQ.json')
   .then(res => res.json())
   .then(faqs => {
+    //const faqs = JSON_FILE
     // Här har vi precis tagit emot hela FAQ.json --> faqs
 
+    BASE_DIV.style.textAlign = "left"
 
+    faqs.forEach(faq => {
+      var h5Tag = document.createElement('h4')
+      h5Tag.innerText = faq['question']
+      BASE_DIV.appendChild(h5Tag)
 
-    var h5Tag = document.createElement('h5')
-    h5Tag.innerText = faqs[0]['question']
-    BASE_DIV.appendChild(h5Tag)
+      pTag = document.createElement('p')
+      pTag.innerText = faq['answer']
+      BASE_DIV.appendChild(pTag)
 
-    pTag = document.createElement('p')
-    pTag.innerText = faqs[0]['answer']
-    BASE_DIV.appendChild(pTag)
-
-
+      lineBreak = document.createElement('br')
+      BASE_DIV.appendChild(lineBreak)
+    })
 
     console.log(faqs[0])
   })
