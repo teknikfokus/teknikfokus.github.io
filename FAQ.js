a12cc134721677c4f5d46133bfcf2fd0b322c303
elinhanstorp <elin.hanstorp@live.com>
2019-01-26 21:26:12 +0100

    changed to english, added eventspage

--- FAQ.js
+++ FAQ.js
@@ -1,7 +1,39 @@
 window.onload = function () {
 
+  initiateFAQ()
+  initiateFAQEng()
 
+
+}
+
+function initiateFAQ() {
   const BASE_DIV = document.getElementById('faq_info_coming_from_FAQ.js')
+  
+  fetch('https://www.teknikfokus.se/FAQ.json')
+  .then(res => res.json())
+  .then(faqs => {
+    //const faqs = JSON_FILE
+    // Här har vi precis tagit emot hela FAQ.json --> faqs
+  
+    BASE_DIV.style.textAlign = "left"
+  
+    faqs.forEach(faq => {
+      var h5Tag = document.createElement('h4')
+      h5Tag.innerText = faq['fraga']
+      BASE_DIV.appendChild(h5Tag)
+  
+      pTag = document.createElement('p')
+      pTag.innerText = faq['svar']
+      BASE_DIV.appendChild(pTag)
+  
+      lineBreak = document.createElement('br')
+      BASE_DIV.appendChild(lineBreak)
+    })
+  })
+}
+
+function initiateFAQEng() {
+  const BASE_DIV = document.getElementById('faq_info_coming_from_FAQeng.js')
 
   fetch('https://www.teknikfokus.se/FAQ.json')
   .then(res => res.json())
@@ -13,11 +45,11 @@ window.onload = function () {
 
     faqs.forEach(faq => {
       var h5Tag = document.createElement('h4')
-      h5Tag.innerText = faq['fraga']
+      h5Tag.innerText = faq['question']
       BASE_DIV.appendChild(h5Tag)
 
       pTag = document.createElement('p')
-      pTag.innerText = faq['svar']
+      pTag.innerText = faq['answer']
       BASE_DIV.appendChild(pTag)
 
       lineBreak = document.createElement('br')
---
+++ events.js
@@ -0,0 +1,52 @@
+window.onload = function () {
+
+
+  const BASE_DIV = document.getElementById('event_infon')
+
+  fetch('https://www.teknikfokus.se/events.json')
+  .then(res => res.json())
+  .then(events => {
+    
+    BASE_DIV.style.textAlign = "left"
+
+    events.forEach(events => {
+      var h5Tag = document.createElement('h4')
+      h5Tag.innerText = events['title (Datummet)'] + '\u00A0' +'◗'+ '\u00A0' 
+      BASE_DIV.appendChild(h5Tag)
+      
+      tab = document.createElement('tr')
+      BASE_DIV.appendChild(tab)
+
+      var h5Tag = document.createElement('h4')
+      h5Tag.innerText =  '◖' + events['name']
+      BASE_DIV.appendChild(h5Tag)
+    
+      lineBreak = document.createElement('br')
+
+      pTag = document.createElement('p')
+      pTag.appendChild(lineBreak)
+      pTag.innerHTML += "<b>Time:</b>" + '\u00A0'+ events['startTime'] + "-" + events['endTime']
+      pTag.appendChild(lineBreak)
+      pTag.innerHTML += "<b>Location:</b> " + '\u00A0'+ events['location']
+      pTag.appendChild(lineBreak)
+      pTag.innerHTML += "<b>Language: </b>" + '\u00A0'+ events['language']
+      BASE_DIV.appendChild(pTag)
+
+
+      // pTag = document.createElement('p')
+      // pTag.innerText = "Location: " + events['location']
+      // BASE_DIV.appendChild(pTag)
+
+      // pTag = document.createElement('p')
+      // pTag.innerText = "Language: " + events['language']
+      // BASE_DIV.appendChild(pTag)
+
+      pTag = document.createElement('p')
+      pTag.innerText = events['description']
+      BASE_DIV.appendChild(pTag)
+
+      
+    })
+
+  })
+}
