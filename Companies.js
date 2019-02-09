50cef94e66b830223bd31a133004fb6501d14eaa
elinhanstorp <elin.hanstorp@live.com>
2019-02-10 00:21:29 +0100

    Companies on two different days shown clearer

--- Companies.js
+++ Companies.js
@@ -2,9 +2,60 @@ window.onload = function () {
 
   const CompanyList_DIV = document.getElementById('compact_company_list.js')
 
+  var compact_12 = document.createElement('div')
+  var head12 = document.createElement('h5')
+  head12.innerText = "Tuesday 12 February"
+  head12.style.fontWeight = "bold"
+  var pageLink = document.createElement('a')
+  pageLink.href = '#'+'Day1'
+  pageLink.appendChild(head12)
+  compact_12.appendChild(pageLink)
+
+  var P12 = document.createElement('p')
+  compact_12.appendChild(P12)
+  CompanyList_DIV.appendChild(compact_12)
+
+
+  var compact_13 = document.createElement('div')
+  var head13 = document.createElement('h5')
+  head13.innerText = "Wednesday 13 February"
+
+  var pageLink = document.createElement('a')
+  pageLink.href = '#'+'Day2'
+  pageLink.appendChild(head13)
+  compact_13.appendChild(pageLink)
+  head13.style.fontWeight = "bold"
+  var P13 = document.createElement('p')
+  compact_13.appendChild(P13)
+  CompanyList_DIV.appendChild(compact_13)
+
+
   const BASE_DIV = document.getElementById('company_info_coming_from_Companies.js')
 
-  let isFirstCompany = true;
+  CompanyCards_12 = document.createElement('div')
+  var div = document.createElement('div')
+  div.id = "Day1"
+  div.className = "card"
+  var h5Tag = document.createElement('h4')
+  h5Tag.innerText = "~ Tuesday 12 February ~"
+  div.appendChild(h5Tag)
+  CompanyCards_12.appendChild(div)
+  BASE_DIV.appendChild(CompanyCards_12)
+
+  CompanyCards_13 = document.createElement('div')
+  var div = document.createElement('div')
+  div.id = "Day2"
+  div.className = "card"
+  var h5Tag = document.createElement('h4')
+  h5Tag.innerText = "~ Wednesday 13 February ~"
+  div.appendChild(h5Tag)
+  CompanyCards_13.appendChild(div)
+  BASE_DIV.appendChild(CompanyCards_13)
+
+
+  let isFirstCompany12 = true;
+  let isFirstCompany13 = true;
+
 
   fetch('https://www.teknikfokus.se/Companies.json')
   .then(res => res.json())
@@ -14,34 +65,19 @@ window.onload = function () {
 
     BASE_DIV.style.textAlign = "left"
     CompanyList_DIV.style.textAlign = "center"
-    var P = document.createElement('p')
+    //var P = document.createElement('p')
+
 
     compInfo.forEach(compInfo => {
+      //creating the companycards:
       var div = document.createElement('div')
       div.id = compInfo['logoName']
       div.className = "card"
-      BASE_DIV.appendChild(div)
+      //CompanyCards_13.appendChild(div)
 
       var h5Tag = document.createElement('h4')
       h5Tag.innerText = compInfo['company']
       div.appendChild(h5Tag)
-
-      if(isFirstCompany) {
-        isFirstCompany = false
-      } else {
-        spacer = document.createElement('span')
-        spacer.innerText = '\u00A0' +'|'+ '\u00A0'
-        P.appendChild(spacer)
-      }
-      aaa = document.createElement('a')
-      aaa.href = '#'+ compInfo['logoName']
-      aaa.innerText = compInfo['company']
-
-      P.appendChild(aaa)
-
-
-      CompanyList_DIV.appendChild(P)
-      
 
       var img = document.createElement('img')
       img.className = "img-fluid"
@@ -60,6 +96,35 @@ window.onload = function () {
       p2Tag.innerText = 'Attending the fair: ' + compInfo['Dag'] + ' february'
       div.appendChild(p2Tag)
 
+      date = compInfo["Dag"]
+      spacer = document.createElement('span')
+      if(isFirstCompany12 && date==12) {
+        isFirstCompany12 = false
+      } else if(isFirstCompany13 && date==13) {
+        isFirstCompany13 = false
+      }
+      else {
+        spacer.innerText = '\u00A0' +'|'+ '\u00A0'
+      }
+
+      aaa = document.createElement('a')
+      aaa.href = '#'+ compInfo['logoName']
+      aaa.innerText = compInfo['company']
+
+      //Adding to cards and compactlist dependning on day.
+
+      if(date == 12){
+        P12.appendChild(spacer)
+        P12.appendChild(aaa)
+        CompanyCards_12.appendChild(div)
+
+      } else if (date == 13) {
+         P13.appendChild(spacer)
+         P13.appendChild(aaa)
+         CompanyCards_13.appendChild(div)
+      }
+
+      //CompanyList_DIV.appendChild(P)
     })
-  })
+ })
 }
