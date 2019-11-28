5e007817cd0a7ea58163d30217453dd2ecb4a954
016172 <seolmi@hotmail.se>
2019-11-28 17:21:01 +0100

    Simplified code

--- newsscript.js
+++ newsscript.js
@@ -10,41 +10,38 @@ function printNews() {
      var pageLink = document.createElement('a')
      pageLink.appendChild(head12)
      compact_12.appendChild(pageLink)
-    var P12 = document.createElement('p')
-    compact_12.appendChild(P12)
-    CompanyList_DIV.appendChild(compact_12)
+     var P12 = document.createElement('p')
+     compact_12.appendChild(P12)
+     CompanyList_DIV.appendChild(compact_12)
 
 
-  const BASE_DIV = document.getElementById('company_info_coming_from_Companies.js')
+    const BASE_DIV = document.getElementById('company_info_coming_from_Companies.js')
 
     CompanyCards_12 = document.createElement('div')
     var div = document.createElement('div')
     CompanyCards_12.appendChild(div)
     BASE_DIV.appendChild(CompanyCards_12)
 
-  fetch('https://www.teknikfokus.se/newsInfo.json')
-  .then(res => res.json())
-  .then(compInfo => {
+   fetch('https://www.teknikfokus.se/newsInfo.json')
+   .then(res => res.json())
+   .then(compInfo => {
     compInfo.reverse();
     BASE_DIV.style.textAlign = "left"
     CompanyList_DIV.style.textAlign = "center"
 
-    compInfo.forEach(compInfo => {
+      compInfo.forEach(compInfo => {
       if((compInfo['Header'] && compInfo['Date'] && compInfo['Container']) != null){
-        //creating the companycards:
       var div = document.createElement('div')
       div.id = compInfo['Time']
       div.className = "card"
-      //CompanyCards_13.appendChild(div)
-
+        
       var h5Tag = document.createElement('h4')
       h5Tag.innerText = compInfo['Header']
       div.appendChild(h5Tag)
-
+        
       var h5Tag = document.createElement('p')
       h5Tag.innerText = compInfo['Date']
       div.appendChild(h5Tag)
-
 
       pTag = document.createElement('p')
       pTag.innerText = compInfo['Container']
@@ -53,7 +50,7 @@ function printNews() {
       lineBreak = document.createElement('br')
       div.appendChild(lineBreak)
 
-        CompanyCards_12.appendChild(div)
+      CompanyCards_12.appendChild(div)
 
       }
     })
