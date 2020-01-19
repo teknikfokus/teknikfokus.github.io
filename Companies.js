9b22d390a188841c0cab50cc7e99d07cc02d20c3
016172 <seolmi@hotmail.se>
2020-01-19 19:50:36 +0100

    Update Companies.js

--- Companies.js
+++ Companies.js
@@ -71,7 +71,7 @@ if(day13){
   let isFirstCompany13 = true;
 
 
-  fetch('https://www.teknikfokus.se/non-published.json')
+  fetch('https://www.teknikfokus.se/companies.json')
   .then(res => res.json())
   .then(compInfo => {
     //const faqs = JSON_FILE
