7bd94d1e0b7f66017a95b62493acec3fad12f9da
016172 <seolmi@hotmail.se>
2020-01-19 19:35:05 +0100

    Attempt #1 to merge new attendees with function

--- Companies.js
+++ Companies.js
@@ -71,7 +71,7 @@ if(day13){
   let isFirstCompany13 = true;
 
 
-  fetch('https://www.teknikfokus.se/Companies.json')
+  fetch('https://www.teknikfokus.se/non-published.json')
   .then(res => res.json())
   .then(compInfo => {
     //const faqs = JSON_FILE
