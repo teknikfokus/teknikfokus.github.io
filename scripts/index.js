e589ad07bec1cce76599dcd8ba1be1841d81d099
Ludvig Svedberg <ludvig.svedberg@mac.com>
2020-10-07 19:32:26 +0200

    Improve company carousel

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,65 @@ const classOrder = ["enter", "left", "center", "right", "exit"]
 let companies = []
 let timerDelay = 3000
 
-function moveCompanies(dir) {
-    console.log("MOVING!");
-    let start = (dir > 0) ? 0 : classOrder.length - 1
-    let end = (dir > 0) ? classOrder.length-1 : 0
-    let oldEnd = $(`.carousel .company.${classOrder[end]}`)
-    for (let i = end-dir; i >= 0 && i < classOrder.length; i -= dir) {
-        $(`.carousel .company.${classOrder[i]}`).removeClass(classOrder[i]).addClass(classOrder[i+dir]).unbind()
+function moveForward() {
+    let carousel = $(".carousel");
+    carousel.children().first().removeClass("edge");
+    carousel.find(".company:nth-last-child(2)").addClass("edge");
+    carousel.prepend($(".carousel .company:last").clone())
+    carousel.append($(".carousel .company:last").clone())
+    carousel.children().addClass("moving");
+
+    carousel.find(".center").removeClass("center");
+    carousel.find(`:nth-child(${(carousel.children().length-1)/2})`).addClass("center")
+    
+    setTimeout(() => {
+        carousel.children().last().remove()
+        carousel.children().last().remove()
+        carousel.children().removeClass("moving");
+    }, 1000)
+}
+
+function shuffleArray(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
     }
-    oldEnd.removeClass(classOrder[end]).addClass(classOrder[start])
-    setUpHandlers()
+    return a;
 }
 
-function setUpHandlers() {
-    $(".carousel .company.left").click(() => {
-        moveCompanies(1)
-        clearInterval(loopInterval);
-        loopInterval = setInterval(() => moveCompanies(1), timerDelay);
-    })
-
-    $(".carousel .company.right").click(() => {
-        moveCompanies(-1)
-        clearInterval(loopInterval);
-        loopInterval = setInterval(() => moveCompanies(1), timerDelay);
-    })
+function initCarousel() {
+    let logos = [];
+    $.get("./images/logos/.", function(data) 
+    {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                logos.push(val);
+            } 
+        });
+        logos = shuffleArray(logos)
+        let carousel = $(".carousel");
+        for (let i = 0; i < 50; i++) {
+            companies.push("A");
+        }
+        let amountOfCompanies = Math.floor(companies.length/2)*2 +1
+    
+        for (let i = 0; i < amountOfCompanies; i++) {
+            logo = logos[i];
+            let element = `
+            <div class="company" style="background-image: url('${logo}')">
+            </div>`;
+            carousel.append(element)
+        }
+        carousel.find(`:nth-child(${(amountOfCompanies-1)/2 + 1})`).addClass("center")
+        carousel.children().first().addClass("edge")
+        carousel.children().last().addClass("edge")
+    
+        setInterval(() => moveForward(), timerDelay);
+    });
 }
-setUpHandlers()
-
-// let loopInterval = setInterval(() => moveCompanies(1), timerDelay);
+$(document).ready(() => {
+    initCarousel()
+});
