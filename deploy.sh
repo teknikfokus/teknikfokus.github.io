c17f825725f10392b07e36f4168b9125ef24383e
Ludvig Svedberg <ludvig.svedberg@mac.com>
2020-11-12 19:03:48 +0100

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:teknikfokus/teknikfokus.github.io.git gh-pages
+git push -f git@github.com:teknikfokus/teknikfokus.github.io.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
