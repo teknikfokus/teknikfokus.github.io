19cf86f6e641ddac1e28a3a182cd94e00adbd2c8
Ludvig Svedberg <ludvig.svedberg@mac.com>
2020-12-23 18:23:42 +0100

    Add for-companies content

--- src/views/ForCompanies.vue
+++ src/views/ForCompanies.vue
@@ -1,9 +1,63 @@
 <template>
   <div id="for-companies">
-    <div class="container pt-5">
-      <div class="row text-center">
-        <p class="d-block mx-auto">For Companies.</p>
+    <div class="container pt-5 mt-5">
+      <h3>Teknikfokus 2021 is digital!</h3>
+      <p>
+        At this page you will find all information relevant for companies about registering at Teknikfokus digital platform and how to make students visit your stall. There are also video tutorials on how to set up your stall. 
+      </p>
+
+      <h3>Registration</h3>
+      <p>
+        There are two steps to register at the online platform used at Teknikfokus this year. 
+      </p>
+
+      <h4>Step 1: Registrating to participate in the fair</h4>
+      <p>
+        There are two steps to register at the online platform used at Teknikfokus this year. 
+      </p>
+
+      
+      <h4>Step 2: Create your virtual stall</h4>
+      <p>
+        To participate in Teknikfokus 2021 you need to create a company account at our digital plattform and build your virtual stall. 
+        <br><br>
+        Everything you need to know to build your stall can be seen in the videos below!
+      </p>
+
+      <div class="videos">
+        <div class="video"><iframe src="https://www.loom.com/embed/690ee60be1264383a54f9118b8c0b281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+        <div class="video"><iframe src="https://www.loom.com/embed/1e7f63fef026488fa1575d56188d67ee" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+        <div class="video"><iframe src="https://www.loom.com/embed/4d46733134ba4d78816a72f8c6374477" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+        <div class="video"><iframe src="https://www.loom.com/embed/993b3cb8a0c240508221f3f89e4d710c" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+        <div class="video"><iframe src="https://www.loom.com/embed/e1d6ce72f35f42959f44cc5460ea7ff1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
       </div>
+  
+
+      <h3>Encourage the students to visit your stall </h3>
+      <p>
+        The most important thing is to show you opportunities and how the students can benefit from being a part of your company. Also make sure that you have an interesting presentation for students that visit the fair to explore and expand their knowledge about the job market
+      </p>
+
+      
+
+      <h5>Contact the students</h5>
+      <p>
+        During the fair, students will be able to contact companies via chat or a booked video meeting. However, do not forget that companies are able to contact students as well. It has shown to be highly appreciated by students when companies also initiate the contact. Use this opportunity to contact students you find interesting and achieve a busy stall.
+      </p>
+
+
+      <h5>Summer jobs, master thesis and internships</h5>
+      <p>
+        Many of our most active students visiting the fair are interested in working at your company during their studies or would love to do their master thesis at your company. If you have any jobs of any kind, make sure to register them at the plattform to make them easy for students to see!
+      </p>
+
+
+      <h5>Company giveaways</h5>
+      <p>
+        In connection to the fair, Teknikfous will help you distribute your merchandise and other physical giveaways to students visiting the fair. Make sure you tell your business contact if you have anything to give to the students. Even if you don't have any physical merchandise to give to the students, doing a lottery where the students can win digital prizes such as an opportunity to get their resumés reviewed or digital gift cards can entice the students to visit your company stall and start a chat.
+      </p>
+
+
     </div>
   </div>
 </template>
@@ -16,3 +70,38 @@ export default {
   }
 }
 </script>
+
+
+<style scoped>
+p {
+  margin-bottom: 40px;
+}
+
+h5 {
+  color: #555;
+}
+
+.videos {
+  margin-bottom: 40px;
+}
+
+.video {
+  display: block;
+  background: #222;
+  position: relative; 
+  padding-bottom: 56.25%; 
+  height: 0;
+  /* max-width: 400px; */
+
+  margin-bottom: 50px;
+}
+
+.video iframe {
+  position: absolute; 
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+}
+</style>
