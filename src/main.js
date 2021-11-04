8fd3db39c803b73d93fa2c1061196c23d962c5ec
Joel-Jensen <info@joeljensen.se>
2021-11-04 18:47:53 +0100

    Tailwindcss

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import './css/style.css'
 
 createApp(App).use(router).use(store).mount('#app')
