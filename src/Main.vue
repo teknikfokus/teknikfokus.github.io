59b831eac5da66040b3ebb92b7f0bf9206918c14
Joel-Jensen <info@joeljensen.se>
2021-11-04 17:45:09 +0100

    vue-next upgrade to Vue3

--- src/Main.vue
+++ src/Main.vue
@@ -75,7 +75,7 @@ export default {
   created () {
     window.addEventListener('scroll', this.handleScroll);
   },
-  destroyed () {
+  unmounted () {
     window.removeEventListener('scroll', this.handleScroll);
   },
   mounted() {
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
 
 const routes = [
   {
@@ -93,8 +90,8 @@ const routes = [
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(),
   routes
 })
 
