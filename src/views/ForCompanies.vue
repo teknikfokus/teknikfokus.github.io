7c1df8588dbfff46c5fc9b06124b798f117e97f4
Joel-Jensen <info@joeljensen.se>
2021-11-10 09:45:06 +0100

    Info for companies

--- src/views/ForCompanies.vue
+++ src/views/ForCompanies.vue
@@ -1,14 +1,38 @@
 <template>
-  <div id="for-companies">
-    <div class="container pt-5">
-      <div class="row text-center mb-3">
-        <i class="d-block mx-auto fas fa-tools fa-3x"></i>
-      </div>
-      <div class="row text-center">
-        <p class="d-block mx-auto">This site is being set up</p>
-      </div>
+    <div id="for-companies" class="py-12">
+        <div class="xl:flex xl:items-center">
+            <div class="xl:w-1/2 xl:px-8 space-y-4">
+                <h3 class="text-xl font-semibold">How can you as a company take part?</h3>
+                <p>With our one-day package you are guaranteed a spot on one of the days of the fair. Please note that there are a limited number of spots at the fair, so make sure to sign up as early as possible.</p>
+                <p>To sign up, please contact: <a href="mailto:relations@teknikfokus.se" class="font-semibold">relations@teknikfokus.se</a></p>
+            </div>
+            <div class="mt-6 xl:mt-0 xl:w-1/2 p-6 xl:p-12 bg-blue-primary text-white rounded-xl shadow-lg">
+                <h3 class="text-3xl font-semibold text-center">18 000 SEK</h3>
+                <p class="mt-6">The full-day package includes:</p>
+                <ul class="mt-2 list-disc pl-6">
+                    <li>Showcase area of 6 m 2</li>
+                    <li>A stand-up table</li>
+                    <li>Parking tickets for our guest parking slots</li>
+                    <li>Power outlets</li>
+                    <li>Internet connection</li>
+                    <li>Wardrobe</li>
+                    <li>A company host to help you upon arrival and during the fair</li>
+                    <li>Breakfast and lunch for your company representatives</li>
+                    <li>Access to our fair lounge with coffee and fika</li>
+                    <li>The ability to display and present your company on our website: teknikfokus.se</li>
+                    <li>Attend in the fair's advertisement</li>
+                    <li>2 tickets to the Teknikfokus banquet which will take place on day 1 (the 16th).
+                        Another opportunity to mingle with the company hosts and the project group</li>
+
+                </ul>
+            </div>
+        </div>
+        <div class="text-center mt-20">
+            <h3 class="text-2xl md:text-4xl font-bold">Other ideas on marketing<br/>your company through Teknikfokus?</h3>
+            <p class="text-lg md:text-xl mt-4 text-gray-600">Contact <a href="mailto:events@teknikfokus.se">events@teknikfokus.se</a> and we can discuss your ideas further. <br />We look forward to your suggestions!</p>
+        </div>
+
     </div>
-  </div>
 </template>
 
 <script>
@@ -19,43 +43,3 @@ export default {
   }
 }
 </script>
-
-
-<style scoped>
-p {
-  margin-bottom: 40px;
-}
-
-h3 {
-  color: var(--primary);
-}
-
-h5, h4 {
-  color: var(--primary-dark)
-}
-
-.videos {
-  margin-bottom: 40px;
-  max-width: 750px;
-  margin: auto;
-}
-
-.video {
-  display: block;
-  background: #222;
-  position: relative; 
-  padding-bottom: 56.25%; 
-  height: 0;
-
-  margin-bottom: 50px;
-}
-
-.video iframe {
-  position: absolute; 
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-
-}
-</style>
