2787654799c9ebf9e5153581c9838ffae57b7dc4
Björn <bjorn.tenjepersson@gmail.com>
2021-11-26 12:49:11 +0100

    Added application to be a functionary or coordinator

--- src/views/ForStudents.vue
+++ src/views/ForStudents.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="for-companies">
-    <div class="container pt-5">
-      <div class="row text-center mb-3">
-        <i class="d-block mx-auto fas fa-tools fa-3x"></i>
+    <div class="mx-auto xl:flex py-10">
+      <div class="py-10 xl:py-0">
+        <div class="mx-auto max-w-xl bg-blue-primary rounded-xl shadow-lg text-white p-6 md:p-10 py-14 relative overflow-hidden">
+          <h4 class="text-2xl">Functionary</h4>
+          <div class ="text-gray-200">
+            <p>Do you wanna get in touch companies and at the same time go to fun events and hang with wonderful people from the E- and D-section? Then you should apply as a functionary to our job fair.</p>
+            <p>You will meet company represantives and together with other volunteers go to the very exclusive banquet for free! After the fair is over, there is also a thank you event to all the functionaries that has made Teknikfokus possible.</p>
+            <p class="mt-2"><a href="https://forms.gle/uHLUFTqWVD4WhQLL6">Apply to be a functionary here</a>!</p>
+          </div>
+        </div>
       </div>
-      <div class="row text-center">
-        <p class="d-block mx-auto">This site is being set up</p>
+      
+      <div class="mx-auto max-w-xl bg-blue-primary rounded-xl shadow-lg text-white p-6 md:p-10 py-14 relative overflow-hidden">
+        <h4 class="text-2xl">Coordinator</h4>
+        <div class="text-gray-200">
+        <p>Do you think that it sounds fun to be a functionary but at the same time feel ready for some extra responsibilites?</p>
+        <p>Then you should apply to be a coordinator instead. You will get the same benefits as a functionary but you will also develop skills which  are attractiv in the job market.</p>
+        <p class="mt-2"><a href="https://forms.gle/972okUNiaDMqNmk37">Apply to be a coordinator here</a>!</p>
+        
       </div>
     </div>
   </div>
