42ee912fe98bc8301db9caeaa80eeb6062699faa
Björn <bjorn.tenjepersson@gmail.com>
2021-11-29 14:06:37 +0100

    Edited the text in ForStudents

--- src/views/ForStudents.vue
+++ src/views/ForStudents.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="mx-auto px-1 xl:px-0 grid grid-cols-1 xl:grid-cols-2 xl:max-w-5xl xl:gap-10 py-5 space-y-10 xl:space-y-0">
-        <div class="mx-auto max-w-xl bg-blue-primary rounded-xl shadow-lg text-white p-6 md:p-10 py-14 relative overflow-hidden">
-          <h4 class="text-2xl">Functionary</h4>
-          <div class ="text-gray-200">
-            <p class="pb-3">Do you wanna get in touch companies and at the same time go to fun events and hang with wonderful people from the E- and D-section? Then you should apply as a functionary to our job fair.</p>
-            <p class="pb-3">You will meet company represantives and together with other volunteers go to the very exclusive banquet for free! After the fair is over, there is also a thank you event to all the functionaries that has made Teknikfokus possible.</p>
-            <p><a class="hover:text-white" href="https://forms.gle/uHLUFTqWVD4WhQLL6">Apply to be a functionary here!</a></p>
-          </div>
+      <div class="mx-auto max-w-xl bg-blue-primary rounded-xl shadow-lg text-white p-6 md:p-10 py-14 relative overflow-hidden">
+        <h4 class="text-2xl">Student volunteer</h4>
+        <div class ="text-gray-200">
+          <p class="pb-3">Do you want to get in touch companies and at the same time attend fun events and hang with wonderful people from the E- and D-section? Then you should apply to be a student volunteer at our job fair.</p>
+          <p class="pb-3">You will meet company representatives and together with other volunteers attend the very exclusive banquet for free! After the fair is over, there is also a thank you event to all the volunteers that has made Teknikfokus possible.</p>
+          <p><a class="hover:text-white" href="https://forms.gle/uHLUFTqWVD4WhQLL6">Apply to be a student volunteer here!</a></p>
         </div>
+      </div>
       
       <div class="mx-auto max-w-xl bg-blue-primary rounded-xl shadow-lg text-white p-6 md:p-10 py-14 relative overflow-hidden">
         <h4 class="text-2xl">Coordinator</h4>
         <div class="text-gray-200">
-        <p class="pb-3">Do you think that it sounds fun to be a functionary but at the same time feel ready for some extra responsibilites?</p>
-        <p class="pb-3">Then you should apply to be a coordinator instead. You will get the same benefits as a functionary but you will also develop skills which  are attractiv in the job market.</p>
+        <p class="pb-3">Do you think that it sounds fun to be a student volunteer but at the same time feel ready for some extra responsibilites?</p>
+        <p class="pb-3">Then you should apply to be a coordinator instead. You will get the same benefits as a 3volunteer but you will also develop skills which  are attractiv in the job market.</p>
         <p><a class="hover:text-white" href="https://forms.gle/972okUNiaDMqNmk37">Apply to be a coordinator here!</a></p>
       </div>
     </div>
