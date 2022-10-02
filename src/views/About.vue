0c8c1304c6d815ed7f2d2f3e15f36d181e21efe0
David Bergh <berghdavid@hotmail.com>
2022-10-02 11:06:20 +0200

    Update PG title, LinkedIn and email
    Adds the titles and email links to everyone in the project group.
    There are still a few linkedIn links missing from the following people:
      - Yazan
      - Arvid
      - Edmond
      - Pontus
      - Theo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="about">
     <div class="container mt-5 mb-5">
-      <h2 class="text-center mb-4 text-capitalize font-weight-bold">The project group</h2>
+      <h1 class="header font-medium text-center mb-4 font-weight-bold">The project group</h1>
       <div class="people-holder">
-        <Person v-for="person of people" :info="person" :key="person.name" />
+        <Person v-for="person of projectManagers" :info="person" :key="person.name" />
+      </div>
+      <div class="people-holder">
+        <Person v-for="person of pg" :info="person" :key="person.name" />
       </div>
     </div>
   </div>
@@ -19,12 +22,12 @@ export default {
   },
   data() {
     return {
-      people: [
+      projectManagers: [
         {
           name: "Leo Guldåker",
           position: "Project Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
+          linkedIn: "https://www.linkedin.com/in/leo-guld%C3%A5ker-b058b2226/",
           mail: "leo@teknikfokus.se"
         },
         {
@@ -33,83 +36,85 @@ export default {
           imageUrl: "@/placeholder-person.png",
           linkedIn: "",
           mail: "yazan@teknikfokus.se"
-        },
+        }
+      ],
+      pg: [
         {
           name: "Alex Bringsarve",
-          position: "",
+          position: "Recruitment Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
-          mail: ""
+          linkedIn: "https://www.linkedin.com/in/alex-bringsarve-0a68bb251/",
+          mail: "alex.bringsarve@teknikfokus.se"
         },
         {
           name: "Alex Chamoun",
-          position: "",
+          position: "Business Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
-          mail: ""
+          linkedIn: "https://www.linkedin.com/in/alex-chamoun-276b13208",
+          mail: "alex.chamoun@teknikfokus.se"
         },
         {
           name: "Arvid Carp",
-          position: "",
+          position: "Logistics Manager",
           imageUrl: "@/placeholder-person.png",
           linkedIn: "",
-          mail: ""
+          mail: "arvid@teknikfokus.se"
         },
         {
           name: "David Bergh",
-          position: "",
+          position: "IT Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
-          mail: ""
+          linkedIn: "https://www.linkedin.com/in/david-bergh-5b1213187/",
+          mail: "david@teknikfokus.se"
         },
         {
           name: "Edmond Mentori",
-          position: "",
+          position: "Event Manager",
           imageUrl: "@/placeholder-person.png",
           linkedIn: "",
-          mail: ""
+          mail: "edmond@teknikfokus.se"
         },
         {
           name: "Eira Blaho Mildton",
-          position: "",
+          position: "Business Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
-          mail: ""
+          linkedIn: "https://www.linkedin.com/in/eira-blaho-mildton-33894119a/",
+          mail: "eira@teknikfokus.se"
         },
         {
           name: "Erik Gustavsson",
-          position: "",
+          position: "Financial Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
-          mail: ""
+          linkedIn: "https://www.linkedin.com/in/erik-gustavsson-1b4503215/",
+          mail: "erik@teknikfokus.se"
         },
         {
           name: "Felix Vigren",
-          position: "",
+          position: "Event Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
-          mail: ""
+          linkedIn: "https://www.linkedin.com/in/felix-vigren-a34202227",
+          mail: "felix@teknikfokus.se"
         },
         {
           name: "Måns Bard Nilsson",
-          position: "",
+          position: "Marketing Manager",
           imageUrl: "@/placeholder-person.png",
-          linkedIn: "",
-          mail: ""
+          linkedIn: "https://www.linkedin.com/in/m%C3%A5ns-bard-nilsson-12a890232",
+          mail: "mans@teknikfokus.se"
         },
         {
           name: "Pontus Sjöstedt",
-          position: "",
+          position: "IT Manager",
           imageUrl: "@/placeholder-person.png",
           linkedIn: "",
-          mail: ""
+          mail: "pontus@teknikfokus.se"
         },
         {
           name: "Theo Callmar",
-          position: "",
+          position: "Business Manager",
           imageUrl: "@/placeholder-person.png",
           linkedIn: "",
-          mail: ""
+          mail: "theo@teknikfokus.se"
         }
       ]
     }
@@ -125,5 +130,10 @@ export default {
     flex-wrap: wrap;
     align-items: stretch;
     margin: 0 -20px;
+  }
+  .header {
+    font-weight: bold;
+    color: #000;
+    font-size: 40px;
   }
 </style>
