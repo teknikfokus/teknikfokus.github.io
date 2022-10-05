632ec6207172eb39121dc665e3c3492c35496119
David Bergh <berghdavid@hotmail.com>
2022-10-05 12:01:30 +0200

    Remove old packages

--- src/views/ForCompanies.vue
+++ src/views/ForCompanies.vue
@@ -40,36 +40,10 @@
             </div>
             <div class="xl:w-2/5">
                 <!-- Righty stuff -->
-                <h1>Packages info</h1>
-            </div>
-        </div>
-        <div class="xl:flex xl:items-center mt-16">
-            <div class="xl:w-1/2 xl:px-8 space-y-4">
-                <h3 class="text-xl font-semibold">How can you as a company take part?</h3>
-                <p>With our one-day package you are guaranteed a spot on one of the days of the fair. Please note that there are a limited number of spots at the fair, so make sure to sign up as early as possible.</p>
-                <p>To sign up, please contact: <a href="mailto:relations@teknikfokus.se" class="font-semibold">relations@teknikfokus.se</a></p>
-            </div>
-            <div class="mt-6 xl:mt-0 xl:w-1/2 p-6 xl:p-12 bg-blue-primary text-white rounded-xl shadow-lg">
-                <h3 class="text-3xl font-semibold text-center">- SEK</h3>
-                <h1 class="mt-6 font-semibold text-center">Note: Specifics will soon be visible</h1>
-                <p class="mt-6">The full-day package includes:</p>
-                <ul class="mt-2 list-disc pl-6">
-                    <li>Showcase area of _m<sup>2</sup></li>
-                    <li>A stand-up table</li>
-                    <li>Parking tickets for our guest parking slots</li>
-                    <li>Power outlets</li>
-                    <li>Internet connection</li>
-                    <li>Wardrobe</li>
-                    <li>A company host to help you upon arrival and during the fair</li>
-                    <li>Access to our fair lounge with coffee and fika</li>
-                    <li>The ability to display and present your company on our website: teknikfokus.se</li>
-                    <li>Attendance in the fair's advertisement</li>
-                    <!--
-                        <li>Breakfast and lunch for your company representatives</li>
-                        <li>TODO 2 tickets to the Teknikfokus banquet which will take place on day 1 (the 16th).
-                            Another opportunity to mingle with the company hosts and the project group</li>
-                    -->
-                </ul>
+                <h1>-Some info about signup-</h1>
+                <h1>-Some info about addons-</h1>
+                <h1>-Some link to our pamphlet-</h1>
+                <h1>-Maybe some contact link-</h1>
             </div>
         </div>
 
