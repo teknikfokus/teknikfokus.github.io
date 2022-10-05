29d5e439534f75d642f4dee07f1a508b2dbf6821
JoPoLu <po7853sj-s@student.lu.se>
2022-10-05 23:24:53 +0200

    Minor css changes

--- src/components/PackageDeal.vue
+++ src/components/PackageDeal.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="packageDeal" class="packageDealClass">
-        <div class="relativeblock justify-center align-center py-6 px-6 text-lg md:text-2x1 font-medium rounded-b-md text-white bg-blue-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-primary-light">
+        <div class="relativeblock justify-center align-center py-6 px-6 text-lg md:text-2x1 font-medium rounded-md text-white bg-blue-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-primary-light">
             <span class="position text-3xl">{{info.packageName}}</span>
             <h1 class="text-xl pl-2">{{info.price}}</h1>
             <h1 class="text-2xl mt-2">Includes:</h1>
