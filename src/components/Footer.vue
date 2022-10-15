8bbbd5d3c62242e7dc9852070b5646f55ed5f6e5
David Bergh <berghdavid@hotmail.com>
2022-10-15 12:32:49 +0200

    Reformat Footer
    Make titles in the footer responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,47 @@
 <template>
-  <footer class="absolute w-full bottom-0 text-white bg-blue-primary">
-    <div class="max-w-xxs mx-auto mt-5 mb-1 ">
-      <div class="grid grid-cols-3 gap-5">
+  <footer class="mx-auto grid md:grid-cols-2 lg:grid-cols-4 gap-4 absolute w-full bottom-0 text-white bg-blue-primary py-8">
+    <div class="max-w-xxs m-auto">
+      <img class="w-40 pt-2" src="@/assets/logo/svg/white-logo-white-text.svg" alt="Teknikfokus logo"/>
+    </div>
+    
+    <div class="max-w-xxs m-auto">
+      <h1 class="text-2xl font-bold underline text-center">Social Media</h1>
+      <div class="grid grid-cols-3 gap-5 pt-4">
         <a class="bg-[#ffffff] hover:bg-blue-primary-lightest transition-colors rounded-full h-x w-x flex items-center justify-center text-4xl px-3 hover:no-underline" href="https://www.facebook.com/teknikfokus" target="_blank"><i class="text-blue-primary fab fa-facebook-f "></i></a>
         <a class="bg-[#ffffff] hover:bg-blue-primary-lightest transition-colors rounded-full h-x w-x flex items-center justify-center text-4xl px-3 hover:no-underline" href="https://www.linkedin.com/company/teknikfokus/" target="_blank"><i class="text-blue-primary fab fa-linkedin-in"></i></a>
         <a class="bg-[#ffffff] hover:bg-blue-primary-lightest transition-colors rounded-full h-x w-x flex items-center justify-center text-4xl px-3 hover:no-underline" href="https://www.instagram.com/teknikfokus/" target="_blank"><i class="text-blue-primary fab fa-instagram"></i></a>
       </div>
     </div>
-    <div class="mx-auto max-w-xl bg-blue-primary pb-5 text-base">
-      <div class="grid grid-cols-1 sm:grid-cols-2 text-center">
-        <div class="mx-auto my-6">
-          <span class="font-bold"> Leo Guldåker, Project manager</span><br>
-          <a class="text-white" href="mailto:leo@teknikfokus.se">leo@teknikfokus.se</a>
+
+    <div class="col-span-2 mx-auto bg-blue-primary text-base">
+      <h1 class="text-2xl font-bold underline text-center">Contact us</h1>
+      <div class="grid grid-cols-1 sm:grid-cols-2">
+        <div>
+          <h2 class="text-lg font-bold text-center pt-2">Project managers</h2>
+          <div class="grid grid-cols-1 2xl:grid-cols-2 text-center">
+            <div class="mx-auto p-2">
+              <span class="font-bold">Leo Guldåker</span><br>
+              <a class="text-white" href="mailto:leo@teknikfokus.se">leo@teknikfokus.se</a>
+            </div>
+            <div class="mx-auto p-2">
+              <span class="font-bold">Yazan Al-Aswad</span><br>
+              <a class="text-white" href="mailto:yazan@teknikfokus.se">yazan@teknikfokus.se</a>
+            </div>
+          </div>
         </div>
-        <div class="mx-auto my-6">
-          <span class="font-bold"> Yazan Al-Aswad, Project manager</span><br>
-          <a class="text-white" href="mailto:yazan@teknikfokus.se">yazan@teknikfokus.se</a>
+
+        <div>
+          <h2 class="text-lg font-bold text-center pt-2">Webpage maintainers</h2>
+          <div class="grid grid-cols-1 2xl:grid-cols-2 text-center">
+            <div class="mx-auto p-2">
+              <span class="font-bold">David Bergh</span><br>
+              <a class="text-white" href="mailto:david@teknikfokus.se">david@teknikfokus.se</a>
+            </div>
+            <div class="mx-auto p-2">
+              <span class="font-bold">Pontus Sjöstedt</span><br>
+              <a class="text-white" href="mailto:pontus@teknikfokus.se">pontus@teknikfokus.se</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -23,7 +49,7 @@
 </template>
 
 <script>
-export default {
+  export default {
     name: "Footer"
-}
+  }
 </script>
