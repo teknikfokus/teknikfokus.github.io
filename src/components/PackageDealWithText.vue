a50e82dd54bbc3c50ac615b866773e3ae7a62664
David Bergh <berghdavid@hotmail.com>
2022-10-16 11:48:53 +0200

    Minor refactoring and rewriting

--- src/components/PackageDealWithText.vue
+++ src/components/PackageDealWithText.vue
@@ -1,42 +1,45 @@
 <template>
-    <div id="packageDealWithText" class="packageDealClass">
-        <div class="relativeblock justify-center align-center py-6 px-6 text-lg md:text-2x1 font-medium rounded-b-md rounded-tr-md text-white bg-blue-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-primary-light">
-            <span class="position text-3xl">{{info.packageName}}</span>
-            <h1 class="text-xl pl-2">{{info.price}}</h1>
-            <h1 class="text-2xl mt-2">Includes:</h1>
-            <div class="justify-center align-center">
-                <li v-for="item in info.perks" :key="item">
-                    {{item}}
-                </li>
-            </div>
-            <div class="justify-center align-center py-12">
-                <h3 v-for="item in info.text" :key="item" class="py-3">{{item}}</h3>
-            </div>
-        </div>
+  <div id="packageDealWithText" class="packageDealClass">
+    <div class="relativeblock justify-center align-center py-6 px-6 text-lg font-medium rounded-b-md rounded-tr-md text-white bg-blue-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-primary-light">
+      <span class="position text-3xl">{{info.packageName}}</span>
+      <h1 class="text-xl pl-2">{{info.price}}</h1>
+      <h1 class="text-2xl mt-2">Includes:</h1>
+      <div class="justify-center align-center">
+        <li v-for="item in info.perks" :key="item">
+          {{item}}
+        </li>
+      </div>
+      <div
+        v-for="item in info.text"
+        :key="item"
+        class="py-2"
+        v-html="item">
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "PackageDealWithText",
-        props: {
-            info: {
-                packageName: String,
-                price: String,
-                text: [String],
-                perks: [String],
-            },
-        },
-        methods: {
-            setInfo(newInfo) {
-                alert(newInfo)
-            }
-        }
+  export default {
+    name: "PackageDealWithText",
+    props: {
+      info: {
+        packageName: String,
+        price: String,
+        text: [String],
+        perks: [String],
+      },
+    },
+    methods: {
+      setInfo(newInfo) {
+        alert(newInfo)
+      }
     }
+  }
 </script>
 
 <style scoped>
-.relativeblock {
+  .relativeblock {
     min-height: 650px;
-}
+  }
 </style>
