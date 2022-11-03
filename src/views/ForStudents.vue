fb3aa87e783283e32f19976c50d0e998a6303fb7
JoPoLu <po7853sj-s@student.lu.se>
2022-11-03 17:00:23 +0100

    Added framework for job descriptions and FAQ on for students page

--- src/views/ForStudents.vue
+++ src/views/ForStudents.vue
@@ -4,8 +4,11 @@
       <a href="#About-teknikfokus" class="bg-blue-primary text-white p-6 rounded-xl shadow-lg flex-1 hover:bg-blue-primary-lightest">
         <div>About Teknikfokus</div>
       </a>
-      <a href="#Company-host" class="bg-blue-primary text-white p-6 rounded-xl shadow-lg flex-1 hover:bg-blue-primary-lightest">
-        <div>Company Hosts</div>
+      <a href="#Jobs" class="bg-blue-primary text-white p-6 rounded-xl shadow-lg flex-1 hover:bg-blue-primary-lightest">
+        <div>Jobs</div>
+      </a>
+      <a href="#FAQ" class="bg-blue-primary text-white p-6 rounded-xl shadow-lg flex-1 hover:bg-blue-primary-lightest">
+        <div>FAQ</div>
       </a>
       <a href="#Student-session" class="bg-blue-primary text-white p-6 rounded-xl shadow-lg flex-1 hover:bg-blue-primary-lightest">
         <div>Student Sessions</div>
@@ -23,20 +26,34 @@
       </h2>
     </div>
 
-    <div id="Company-host" class="py-6 content-center"></div>
-    <div class="flex flex-col mx-auto text-lg">
-      <h1 class="mt-12 text-5xl text-center font-bold">Company Host</h1>
-      <h2 class="mt-12 text-center">
-        During the fair the companies will need help when setting up their showcase area. As some companies have not been at LTH before they might also have other questions regarding E-huset. The job of a company host is to be their contact person prior to and during the fair as well as to help their company representatives while attending the fair.
-      </h2>
-      <h2 class="mt-12 text-center">
-        To show our thanks to everyone who contributed to Teknikfokus every host will get a ticket to our annual banquet. The work of a Company Host is greatly appreciated by the companies and is a great way to get in contact with those you are helping.
-      </h2>
-      <button class="mt-6 mx-auto bg-blue-primary text-white text-xl font-bold p-8 rounded-xl shadow-lg hover:bg-blue-primary-lightest">
-        🎉 Apply for Company Host 🎉
-      </button>
+    <div id="Jobs" class="py-6"></div>
+    <h1 class="mt-12 text-5xl text-center font-bold">Official job decriptions</h1>
+    
+    <div class="pt-5 pb-5">
+      <div class="max-w-510xl mx-auto" >
+        <div id="faq-accordion" class="divide-y-2 divide-gray-200">
+          <Question v-for="(question, index) in jobs" :key="index" :info="question" :index="index"></Question>
+          <ErrorMessage v-if="questions <= 0" text="No frequently asked questions found." />
+        </div>
+      </div>
     </div>
+    <div id="How-to-apply" class="py-6"></div>
+    <h1 class="mt-12 text-5xl text-center font-bold">Aplication of interest</h1>
+    <h3 class="pt-4 text-center text-xl">For any questions or additional inquiries please contact <a class="font-bold" href="mailto:relations@teknikfokus.se">relations@teknikfokus.se</a>.</h3>
+    <div class="pt-4 max-w-5xl h-700 mx-auto">
+      <iframe class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" src="https://docs.google.com/forms/d/e/1FAIpQLSdp6eWNkCvD-14nRp5shKeSuzMV1pI78jmycoNp5SwML5lUKg/viewform?embedded=true" width="500" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+    <div id="FAQ" class="py-6"></div>
+    <h1 class="mt-12 text-5xl text-center font-bold">FAQ</h1>
 
+    <div id="faq" class="pt-5 pb-5">
+      <div class="max-w-510xl mx-auto" >
+        <div id="faq-accordion" class="divide-y-2 divide-gray-200">
+          <Question v-for="(question, index) in questions" :key="index" :info="question" :index="index"></Question>
+          <ErrorMessage v-if="questions <= 0" text="No frequently asked questions found." />
+        </div>
+      </div>
+    </div>
     <div id="Student-session" class="py-6 content-center"></div>
     <div class="flex flex-col mx-auto text-lg">
       <h1 class="mt-12 text-5xl text-center font-bold">Student Sessions</h1>
@@ -49,9 +66,56 @@
     </div>
   </div>
 </template>
-
 <script>
+import Question from '../components/Question'
+const jobs = [
+    {
+      question: "Company Host",
+      answer: "<p>text explaining company host</p>"
+    },
+    {
+      question: "Night Watch",
+      answer: "<p>Jar Jar Binks is a sith Lord</p>"
+    },
+    {
+      question: "Garderob/Lounge/FrukostVärd(TODO FIX NAME)",
+      answer: "<p>TODO</p>"
+    },
+    {
+      question: "Electricity/Internet(TODO FIX NAME)",
+      answer: "<p>Data X Electricity elctric boogalo edition</p>"
+    },
+    {
+      question: "Shuttle Host(TODO FIX NAME)",
+      answer: "<p>TODO</p>"
+    },
+    {
+      question: "Photographer(TODO FIX NAME)",
+      answer: "<p>TODO</p>"
+    },
+    {
+      question: "Kontaktsamtal Koordinator(TODO FIX NAME)",
+      answer: "<p>TODO</p>"
+    },
+  ];
+
+  const questions = [
+    {
+      question: "I wonder about icecream and stuffs",
+      answer: "<p>text explaining company host</p>"
+    },
+    {
+      question: "Where can I get a cool hat like Felixs??!?!",
+      answer: "<p>Jar Jar Binks is a sith Lord</p>"
+    },
+  ];
   export default {
-    name: 'ForStudents'
+    name: 'ForStudents',
+    components: {
+      Question
+    },
+    setup() {
+      return {jobs, questions}
+    }
   }
 </script>
