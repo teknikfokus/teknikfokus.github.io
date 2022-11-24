29738b7b10cd21476104f1fbacae50c3c2c1833f
David Bergh <berghdavid@hotmail.com>
2022-11-24 16:35:31 +0100

    Point API to dev backend

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
 import Vuex from 'vuex'
 import { jsonapiModule } from 'jsonapi-vuex'
 import axios from 'axios'
-export const endpoint = "https://api.teknikfokus.se"
+export const endpoint = "https://dev.teknikfokus.se"
 
 const api = axios.create({
-    baseURL: endpoint+"/v1",
-    headers: {
-        'Content-Type': 'application/vnd.api+json'
-    },
+  baseURL: endpoint,
+  headers: {
+    'Content-Type': 'application/vnd.api+json'
+  },
 })
 
 export default new Vuex.Store({
