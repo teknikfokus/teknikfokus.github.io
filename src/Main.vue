85cad2e7b870bade8afe3bd7e34005fdc3f5a458
David Bergh <berghdavid@hotmail.com>
2023-01-18 20:44:49 +0100

    Move Student Sessions to separate section

--- src/Main.vue
+++ src/Main.vue
@@ -137,6 +137,16 @@ const nav = [
     ],
   },
   {
+    name: 'Student Sessions',
+    href: '/sessions',
+    sub_items: [
+      {
+        name: 'About',
+        href: '#About-sessions'
+      }
+    ],
+  },
+  {
     name: 'For Companies',
     href: '/forcompanies',
     sub_items: [
