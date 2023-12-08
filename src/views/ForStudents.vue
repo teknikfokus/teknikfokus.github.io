aa1301fe339e8d225d0e99b123e9f8c9b6784e39
Noor E Mustafa <noor_eidirhn.mustafa6827@student.lu.se>
2023-12-08 20:03:15 +0100

    pa

--- src/views/ForStudents.vue
+++ src/views/ForStudents.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="for-companies" class="scroll-smooth">
-        <!-- <h1  class="text-4xl text-left p-12 text-animate  xl:px-24 container  content border-2  text-white  font-bold bg-blue-primary ">For Students </h1>
+    <!-- <h1  class="text-4xl text-left p-12 text-animate  xl:px-24 container  content border-2  text-white  font-bold bg-blue-primary ">For Students </h1>
    -->
-  
-<div id="About-teknikfokus" class=" py-20 bg-blue-primary   ">
 
-        <div class="flex content container   xl:px-24">
-                <div class="align-center xl:w-1/2 ">
-                  <div class=" p-10 text-lg xl:pr-8 ">
-                    <h1 class="text-4xl text-white  text-left   font-bold hidMe ">For Student</h1>
-                  </div>
-                </div>
+    <div id="About-teknikfokus" class=" py-20 bg-blue-primary   ">
 
-
+      <div class="flex content container   xl:px-24">
+        <div class="align-center xl:w-1/2 ">
+          <div class=" p-10 text-lg xl:pr-8 ">
+            <h1 class="text-4xl text-white  text-left   font-bold hidMe ">For Student</h1>
           </div>
-</div>
+        </div>
 
 
-<!-- 
+      </div>
+    </div>
+
+
+    <!-- 
 <div class="flex content container  xl:px-24">
 <div class="align-center   xl:w-1/2 ">
   <div class=" p-10 text-lg xl:pr-8 ">
@@ -26,23 +26,27 @@
       Every great career fair needs awesome hosts and Teknikfokus is no different. We will need lots of help with setting things up for the fair and with answering all kinds of questions about the fair in the info desk. We will also need company host that ensure that the companies feel welcome.     </h2>
     <br> -->
 
-    <div class=" student-background bg-gray-trans flex-row xl:flex xl:items-center "> 
+    <div class=" student-background bg-gray-trans flex-row xl:flex xl:items-center ">
 
       <div class="flex content container xl:px-24">
-      <div class="align-center xl:w-1/2 ">
-        <div class=" p-10 text-lg xl:pr-8 ">
-          <h1 class="text-4xl text-white  text-left   font-sans md:font-serif hidMe ">What is Teknikfokus?</h1>
-          <h2 class="mt-4 text-white  hidMe ">
-            Teknikfokus is a career fair aimed towards students studying at the E and D guilds. During the 13:th and 14:th of February ~70 companies will be at E-huset to present what they are all about. This is your chance to get in contact with future employers through summer jobs, master’s theses and much more.
-          </h2>
-          <br>
-          <h2 class="mt-4 text-white hidMe  ">
-            Along with the fair there will also be lunch lectures, pubs and student sessions happening all around E-huset. Follow the links below to read more about participating in Student Sessions and applying for Host.</h2>
-          <br>
-          <h2 class="mt-4 text-white  hidMe ">See you at the fair!</h2>
-            
-          <!-- <h2 class="text-4xl font-bold text-center m-2">Host Application is opening soon!</h2> -->
-<!-- 
+        <div class="align-center xl:w-1/2 ">
+          <div class=" p-10 text-lg xl:pr-8 ">
+            <h1 class="text-4xl text-white  text-left   font-sans md:font-serif hidMe ">What is Teknikfokus?</h1>
+            <h2 class="mt-4 text-white  hidMe ">
+              Teknikfokus is a career fair aimed towards students studying at the E and D guilds. During the 13:th and
+              14:th of February ~70 companies will be at E-huset to present what they are all about. This is your chance
+              to get in contact with future employers through summer jobs, master’s theses and much more.
+            </h2>
+            <br>
+            <h2 class="mt-4 text-white hidMe  ">
+              Along with the fair there will also be lunch lectures, pubs and student sessions happening all around
+              E-huset. Follow the links below to read more about participating in Student Sessions and applying for Host.
+            </h2>
+            <br>
+            <h2 class="mt-4 text-white  hidMe ">See you at the fair!</h2>
+
+            <!-- <h2 class="text-4xl font-bold text-center m-2">Host Application is opening soon!</h2> -->
+            <!-- 
            <div class="flex flex-col pt-8 md center xl:w-96 xl:align-left">
             <div class="py-2 hidden xl:grid">
               <cool-button text="Host Application" href="https://www.teknikfokus.se/host"/>
@@ -57,73 +61,75 @@
               <cool-button2 text="Student Sessions" href="https://www.teknikfokus.se/sessions"/>
             </div> 
           </div>  -->
+          </div>
         </div>
-      </div>
 
-      <!-- Righty stuff -->
-       <!-- <div class="hidden xl:grid xl:w-1/2 p-10">
+        <!-- Righty stuff -->
+        <!-- <div class="hidden xl:grid xl:w-1/2 p-10">
         <div class="max-w-full mx-auto xl:visible">
           <img src="../assets/images/massa2.jpg" class="max-w-full" alt="Group of attendees" />
         </div>
       </div>  -->
+      </div>
+
+
+
+
     </div>
 
 
+    <div class=" bg-blue-primary-lightest flex-row xl:flex xl:items-center ">
+
+      <div class="flex content container  xl:px-24">
+        <div class="align-center   xl:w-1/2 ">
+          <div class=" p-10 text-lg xl:pr-8 ">
+            <h1 class="text-5xl font-sans md:font-serif  text-white text-left hidMe ">We had fun, come and join us.</h1>
+            <h2 class="mt-4  text-white hidMe">
+              Every great career fair needs awesome hosts and Teknikfokus is no different. We will need lots of help with
+              setting things up for the fair and with answering all kinds of questions about the fair in the info desk. We
+              will also need company host that ensure that the companies feel welcome. </h2>
+            <br>
 
 
-    </div>
 
+            <!-- <h2 class="text-4xl font-bold text-center m-2">Host Application is opening soon!</h2> -->
 
- <div class=" bg-blue-primary-lightest flex-row xl:flex xl:items-center "> 
-
-<div class="flex content container  xl:px-24">
-<div class="align-center   xl:w-1/2 ">
-  <div class=" p-10 text-lg xl:pr-8 ">
-    <h1 class="text-5xl font-sans md:font-serif  text-white text-left hidMe ">We had fun, come and join us.</h1>
-    <h2 class="mt-4  text-white hidMe">
-      Every great career fair needs awesome hosts and Teknikfokus is no different. We will need lots of help with setting things up for the fair and with answering all kinds of questions about the fair in the info desk. We will also need company host that ensure that the companies feel welcome.     </h2>
-    <br>
-
-  
-      
-    <!-- <h2 class="text-4xl font-bold text-center m-2">Host Application is opening soon!</h2> -->
-
-     <div class="flex flex-col pt-8  md center xl:w-96 xl:align-left hidMe  ">
-      <div class="py-2  hidden xl:grid">
-        <cool-button text="Host Application" href="https://www.teknikfokus.se/host"/>
-      </div>
-      <div class="py-2 grid xl:hidden">
-        <cool-button2 text="Host Application" href="https://www.teknikfokus.se/host"/>
-      </div>
-      <!-- <div class="py-2 hidden xl:grid">
+            <div class="flex flex-col pt-8  md center xl:w-96 xl:align-left hidMe  ">
+              <div class="py-2  hidden xl:grid">
+                <cool-button text="Host Application" href="https://www.teknikfokus.se/host" />
+              </div>
+              <div class="py-2 grid xl:hidden">
+                <cool-button2 text="Host Application" href="https://www.teknikfokus.se/host" />
+              </div>
+              <!-- <div class="py-2 hidden xl:grid">
         <cool-button text="Student Sessions" href="https://www.teknikfokus.se/sessions"/>
       </div>
       <div class="py-2 grid xl:hidden">
         <cool-button2 text="Student Sessions" href="https://www.teknikfokus.se/sessions"/>
       </div> -->
-    </div> 
-  </div>
-</div>
+            </div>
+          </div>
+        </div>
 
-<!-- Righty stuff -->
-<div class="hidden xl:grid  xl:w-1/2  hidMe ">
-  <div class="max-w-full mx-auto xl:visible py-11">
-    <img src="../assets/images/fun.jpg" class="max-w-full" alt="Group of attendees" />
- 
-  </div>
-  
-</div>
-</div>
+        <!-- Righty stuff -->
+        <div class="hidden xl:grid  xl:w-1/2  hidMe ">
+          <div class="max-w-full mx-auto xl:visible py-11">
+            <img src="../assets/images/fun.jpg" class="max-w-full" alt="Group of attendees" />
 
+          </div>
 
+        </div>
+      </div>
 
 
-</div>
+
+
+    </div>
 
     <!-- <div id="Students-FAQ" class="py-6"></div> -->
     <h1 class=" text-5xl text-center font-bold bg-gray-trans p-5  ">FAQ</h1>
     <div id="students-faq" class="pt-5 pb-5 bg-gray-trans">
-      <div class="max-w-510xl mx-auto hidMe" >
+      <div class="max-w-510xl mx-auto hidMe">
         <div id="students-faq-accordion" class="divide-y-2 content container divide-gray-200 ">
           <Question v-for="(question, index) in questions" :key="index" :info="question" :index="index"></Question>
           <ErrorMessage v-if="questions <= 0" text="No frequently asked questions found." />
@@ -134,56 +140,56 @@
 </template>
 
 <script>
-  import Question from '../components/Question';
-  import CoolButton from '../components/CoolButton.vue';
-  import CoolButton2 from '../components/CoolButton2.vue';
-  import ErrorMessage from '../components/ErrorMessage'
-  import scroll_to_hash from '../router/scroll_to_hash';
+import Question from '../components/Question';
+import CoolButton from '../components/CoolButton.vue';
+import CoolButton2 from '../components/CoolButton2.vue';
+import ErrorMessage from '../components/ErrorMessage'
+import scroll_to_hash from '../router/scroll_to_hash';
 
-  const questions = [
-    {
-      question: "Who organizes Teknikfokus?",
-      answer: "<p>Teknikfokus is arranged by a project group consisting of students from the four programs at the E- and D-faculties  at LTH, who in parallel with their studies prepare the fair and all our events. The goal is to have a fair where everyone feels welcome, and where all attendees feel satisfied with the fair. In addition, the fair would never be able to take place if it wasn’t for all the students that volunteer at the fair. If you have any questions regarding the project group or our volunteers you can contact our project leaders, Samuel Högfeldt, <a class=\"font-bold\" href=\"mailto:samuel@teknikfokus.se\">samuel@teknikfokus.se</a> or Alen Mahmutovic, <a class=\"font-bold\" href=\"mailto:alen@teknikfokus.se\">alen@teknikfokus.se</a>. You can read about everyone in the project group under <a class=\"font-bold\" target=\"_blank\" href=https://www.teknikfokus.se/about#PG>About us</a>.</p>"
-    },
-    {
-      question: "When is Teknikfokus?",
-      answer: "<p>The fair will take place between 09:00 and 16:30, the 13th & 14th of February.</p>"
-    },
-    {
-      question: "Where will Teknikfokus take place?",
-      answer: `
+const questions = [
+  {
+    question: "Who organizes Teknikfokus?",
+    answer: "<p>Teknikfokus is arranged by a project group consisting of students from the four programs at the E- and D-faculties  at LTH, who in parallel with their studies prepare the fair and all our events. The goal is to have a fair where everyone feels welcome, and where all attendees feel satisfied with the fair. In addition, the fair would never be able to take place if it wasn’t for all the students that volunteer at the fair. If you have any questions regarding the project group or our volunteers you can contact our project leaders, Samuel Högfeldt, <a class=\"font-bold\" href=\"mailto:samuel@teknikfokus.se\">samuel@teknikfokus.se</a> or Alen Mahmutovic, <a class=\"font-bold\" href=\"mailto:alen@teknikfokus.se\">alen@teknikfokus.se</a>. You can read about everyone in the project group under <a class=\"font-bold\" target=\"_blank\" href=https://www.teknikfokus.se/about#PG>About us</a>.</p>"
+  },
+  {
+    question: "When is Teknikfokus?",
+    answer: "<p>The fair will take place between 09:00 and 16:30, the 13th & 14th of February.</p>"
+  },
+  {
+    question: "Where will Teknikfokus take place?",
+    answer: `
         <p>At Ole Römers väg 3, inside E-huset.</p>
         <iframe class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1001.2498072316307!2d13.209636197891264!3d55.711069045083036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465397ca158a6165%3A0x7f9339d3824e88ee!2sOle%20R%C3%B6mers%20v%C3%A4g%203%2C%20223%2063%20Lund!5e0!3m2!1ssv!2sse!4v1665410976182!5m2!1ssv!2sse" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">E-huset LTH Ole Römers väg 3.</iframe>
       `
-    },
-    {
-      question: "Who will be attending?",
-      answer: "<p>Bachelors, Masters and PhD-Students from the respective forementioned engineering programs, as well as 60 to 80 different companies within the relevant fields. All attending companies are displayed at the <a target='_blank' class='font-bold' href='https://www.teknikfokus.se/attending'>Attending Companies</a> page.</p>"
-    },
-    {
-      question: "Who should you contact if you have questions about the companies?",
-      answer: "<p>If you have questions as a company or regarding a company attending Teknikfokus, please contact us at <a class=\"font-bold\" href=\"mailto:relations@teknikfokus.se\">relations@teknikfokus.se</a>. Also, feel free to reach out to us using contact information under <a class=\"font-bold\" target=\"_blank\" href=https://www.teknikfokus.se/about#PG>About us</a>. If you have comments about the companies invited, or feel that a company doesn’t represent Teknikfokus in a good way, contact one of our project managers at <a class=\"font-bold\" href=\"mailto:projektledare@teknikfokus.se\">projektledare@teknikfokus.se</a>.</p>"
-    }
-  ];
-  export default {
-    name: 'ForStudents',
-    components: {
-      Question,
-      ErrorMessage,
-      CoolButton,
-      CoolButton2
-    },
-    setup() {
-      return {questions}
-    },
-    mounted() {
+  },
+  {
+    question: "Who will be attending?",
+    answer: "<p>Bachelors, Masters and PhD-Students from the respective forementioned engineering programs, as well as 60 to 80 different companies within the relevant fields. All attending companies are displayed at the <a target='_blank' class='font-bold' href='https://www.teknikfokus.se/attending'>Attending Companies</a> page.</p>"
+  },
+  {
+    question: "Who should you contact if you have questions about the companies?",
+    answer: "<p>If you have questions as a company or regarding a company attending Teknikfokus, please contact us at <a class=\"font-bold\" href=\"mailto:relations@teknikfokus.se\">relations@teknikfokus.se</a>. Also, feel free to reach out to us using contact information under <a class=\"font-bold\" target=\"_blank\" href=https://www.teknikfokus.se/about#PG>About us</a>. If you have comments about the companies invited, or feel that a company doesn’t represent Teknikfokus in a good way, contact one of our project managers at <a class=\"font-bold\" href=\"mailto:projektledare@teknikfokus.se\">projektledare@teknikfokus.se</a>.</p>"
+  }
+];
+export default {
+  name: 'ForStudents',
+  components: {
+    Question,
+    ErrorMessage,
+    CoolButton,
+    CoolButton2
+  },
+  setup() {
+    return { questions }
+  },
+  mounted() {
     scroll_to_hash();
 
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add('showMe');
-        } 
+        }
       });
     }, {
       rootMargin: '0px',
@@ -193,24 +199,24 @@
     const hiddenElements = document.querySelectorAll('.hidMe');
     hiddenElements.forEach((el) => observer.observe(el));
   }
-  }
+}
 
 
 
 </script>
 
 <style scoped>
-  .student-background {
-    background-image: url('../assets/images/massa2020blur.jpg');
-    background-position: center;
-    background-size: cover;
-    background-attachment: fixed;
-    min-height: 70vh;
-   
-  
-  }
+.student-background {
+  background-image: url('../assets/images/massa2020blur.jpg');
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+  min-height: 70vh;
 
-  .hidMe {
+
+}
+
+.hidMe {
   transform: translateX(-30%);
   opacity: 0;
   transition: transform 1s ease-out, opacity 1s ease-out;
@@ -220,7 +226,4 @@
   transform: translateX(0);
   opacity: 1;
 }
-
-
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,29 +2,28 @@
   <div id="home">
 
 
-    <div
-      class=" p-10 hero-background  bg-center bg-cover text-center text-xl md:text-2xl text-white text-shadow-lg uppercase">
+    <div class=" py-5 hero-background   text-center text-xl md:text-2xl text-white  uppercase">
 
 
 
-      <div class=" m-20">
-        <CountDown :firstDate="firstDate" :secondDate="secondDate" />
-      </div>
 
-      <div class="  container  flex  ">
+      <CountDown :firstDate="firstDate" :secondDate="secondDate" />
 
 
+      <div class=" my-20 container  flex  ">
 
-        <div class=" xl:w-1/2  flex-1  flex-col items-center justify-center     ">
 
-          <div class="  flex  flex-col  items-center  justify-center p-2 ">
+
+        <div class=" xl:w-1/2  flex-1  flex-col items-center justify-center    ">
+
+          <div class="  flex  flex-col  items-center  justify-center py-2 ">
             <div class="xl:w-1/2  ">
               <img src="../assets/logo/svg/white-logo-white-text.svg" class="max-w-full " alt="Teknikfokus" />
             </div>
 
-            <div class=" m-2">
-              <h2 class=" font-normal md:text-3xl text-center">Career fair</h2>
-              <h2 class="text-2xl italic font-normal mt-2 text-center">Where the present meets the future</h2>
+            <div class=" py-2">
+              <h2 class=" py-2 font-normal  text-1xl  text-center">Career fair</h2>
+              <h2 class="  py-2 text-1xl font-normal  text-center">Where the present meets the future</h2>
             </div>
 
 
@@ -41,13 +40,13 @@
 
 
 
-        <div class="  xl:w-1/2   flex-1  flex-col items-center justify-center     ">
+        <div class="  xl:w-1/2   flex-1  flex-col items-center justify-center    ">
 
-          <h1 class=" font-normal  md:text-6xl p-2   ">Next fair</h1>
-          <h2 class=" text-2xl italic font-normal  p-2"> 15th &amp; 16th of February, 2024!</h2>
-          <h2 class="text-2xl italic font-normal  p-2">09:00-16:30</h2>
-          <h2 class="text-2xl italic font-normal  p-2"> E-huset LTH Ole Römers väg 3 Lund</h2>
-          <div class="social-icons">
+          <h1 class=" font-bold  text-5xl  ">Next fair</h1>
+          <h2 class="text-1xl font-normal py-2"> 15th &amp; 16th of February, 2024!</h2>
+          <h2 class="text-1xl font-normal   py-2">09:00-16:30</h2>
+          <h2 class="text-1xl font-normal  py-2"> E-huset LTH Ole Römers väg 3 Lund</h2>
+          <div class=" py-2 social-icons">
             <a href="https://www.linkedin.com/company/teknikfokus/" target="_blank"
               class="fa fa-linkedin p-2 no-underline hover:no-underline focus:no-underline active:no-underline"></a>
             <a href="https://www.instagram.com/teknikfokus/" target="_blank"
@@ -57,16 +56,15 @@
           </div>
 
 
-          <div class="content   container p-2">
+          <div class="  container  py-2">
             <button
-              class=" responsive-button  bg-white  text-black font-normal m-6 py-2 px-4 rounded-full focus:outline-none"
+              class=" responsive-button  bg-white  text-black font-normal  py-2 px-4 rounded-full focus:outline-none"
               @click="goToForStudents">
               For Students
             </button>
 
 
-            <button
-              class=" responsive-button bg-white  text-black font-normal m-6 py-2 px-4 rounded-full focus:outline-none"
+            <button class=" responsive-button bg-white  text-black font-normal py-2 px-4 rounded-full focus:outline-none"
               @click="goToForCompany">
               For Companies
             </button>
@@ -163,7 +161,9 @@ export default {
   background-image: url('../assets/images/tek33.jpg');
   background-position: center;
   background-size: cover;
-  height: 100vh;
+
+
+  min-height: 100vh;
 }
 
 
