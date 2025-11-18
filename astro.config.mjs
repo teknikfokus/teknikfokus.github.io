455510eea36b5f3bfdeda21737a36dce839946aa
Dag Hemberg <dag.hemberg@gmail.com>
2025-11-18 23:01:01 +0100

    upgrade astro, add svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.teknikfokus.se/",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), svelte()]
 });
